661f3cd1984d077c1b97d340165903cf495a498e
Govind Goyal <govindgoyal4440@gmail.com>
2025-06-17 17:50:13 +0530

    second commit

--- Shimmer.js
+++ Shimmer.js
@@ -63,13 +63,7 @@ export default function Shimmer(){
                     <div className="w-full h-6  bg-gray-300 mt-2 ml-10"></div>
             </div>
 
-            <div className=" w-[280px] mb-2">
-                <div className="w-70 h-45 rounded-xl bg-gray-300 ml-10"></div>
-                <div className="w-[95%] mx-auto mt-3"></div>
-                    <div className="w-full h-6 bg-gray-300 ml-10"></div>
-                    <div className="w-full h-6 bg-gray-300 mt-2 ml-10"></div>
-                    <div className="w-full h-6  bg-gray-300 mt-2 ml-10"></div>
-            </div>
+            
 
             </div>
         </>
